47faef471225cb26eccb6a2cb5e60e6b2ad9d153
Daniel Hemphill <danielhemp@users.noreply.github.com>
2021-11-23 14:28:43 -0600

    but me a coffee

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -103,6 +103,7 @@ const Footer = class extends React.Component {
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
+                <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="danielhemphill" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
               </div>
             </div>
           </div>
